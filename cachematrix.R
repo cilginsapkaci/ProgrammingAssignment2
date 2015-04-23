ff07e21077cb6b52c8fbfc58ede877ea36aabceb
CilginSapkaci <alotte@gmail.com>
2015-04-23 14:09:03 -0400

    draft assignment to update

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,73 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following two functions calculate and cache the inverse of an invertible 
+## matrix. 
+## The first function (makeCacheMatrix) stores a list of functions (set, get, 
+## setinv, getinv) 
+## The second function uses the list of functions stored by makeCacheMatrix to
+## return the inverse of the matrix either from the cache or by computation.
 
-## Write a short comment describing this function
+
+## This function is a list of four functions. getmatr() returns the matrix, 
+## setmatr() replaces the matrix with the entered matrix, setinv() computes 
+## and caches the inverse and getinv() returns the inverse. 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        p <- NULL
+        setmatr <- function(y) {
+            x <<- y
+            p <<- NULL
+        }
+        getmatr <- function() x
+        setinv <- function(solve) p <<- solve
+        getinv <- function() p
+        list(setmatr = setmatr, getmatr = getmatr,
+             setinv = setinv,
+             getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+
+## The cacheSolve takes the matrix returned by makeCacheMatrix function and 
+## first checks if the inverse is already cached and unchanged.  In that case, it
+## yields the inverse from the cache.  
+## If the cache does not have the inverse, it gets the matrix to be inverted 
+## and solves for the inverse and stores this inverse as the cached inverse.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      p <- x$getinv()
+       if(!is.null(p)) {
+             message("getting cached data")
+               return(p)
+           }
+       data <- x$getmatr()
+       p <- solve(data, ...)
+       x$setinv(p)
+       p     
+
+#   Example:
+#   b <- matrix(1:4, 2, 2)
+#   test1 <- makeCacheMatrix(b)
+#   test1$getmatr()
+#        [,1] [,2]
+#   [1,]    1    3
+#   [2,]    2    4
+#   test1$getinv()
+#   NULL
+#   cacheSolve(test1)
+#        [,1] [,2]
+#   [1,]   -2  1.5
+#   [2,]    1 -0.5
+#   test1$getinv()
+#        [,1] [,2]
+#   [1,]   -2  1.5
+#   [2,]    1 -0.5
+#   cacheSolve(test1)
+#   getting cached data
+#        [,1] [,2]
+#   [1,]   -2  1.5
+#   [2,]    1 -0.5
+#    test1$getinv()
+#      [,1] [,2]
+# [1,]   -2  1.5
+# [2,]    1 -0.5     
+    
 }
